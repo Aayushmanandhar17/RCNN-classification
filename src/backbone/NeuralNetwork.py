8b718b370d5bba50c38fc44dfab5449b53c13a7f
Adammanandhar <aayush.manandhar17@gmail.com>
2023-12-03 14:31:36 +0000

    created a resnet_classification model

--- src/backbone/NeuralNetwork.py
+++ src/backbone/NeuralNetwork.py
@@ -4,26 +4,26 @@ import torch.nn as nn
 import torchvision.models as models
 
 class ClassificationRCNN(nn.Module):
-    def __init__(self, num_classes=100):
+    def __init__(self, num_classes=80):
         super(ClassificationRCNN, self).__init__()
-        # Load a pre-trained MobileNetV2 model and use its features
-        mobilenet = models.mobilenet_v2(pretrained=True)
-        self.backbone = mobilenet.features
-
-        # Add custom layers for classification
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.classifier = nn.Sequential(
-            nn.Linear(1280, 1024),  # MobileNetV2's last channel is 1280
-            nn.ReLU(),
+        faster_rcnn=models.detection.fasterrcnn_resnet50_fpn_v2(pretrained=True)
+        #only extracting the ResNet50 backbone
+        self.resnet_backbone=faster_rcnn.backbone.body
+        
+        self.ffn=nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(2048,512),
+            nn.Relu(),
             nn.Dropout(0.5),
-            nn.Linear(1024, num_classes)
+            nn.Linear(512, num_classes)
         )
-
+        
     def forward(self, x):
-        x = self.backbone(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
+        x = self.resnet_backbone(x)
+        #average pooling
+        x= torch.mean(x, dim=[2,3])
+        x=self.ffn(x)
         return x
-
-
+    
+        
+        
